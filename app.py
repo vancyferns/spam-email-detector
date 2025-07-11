08dae9df67287135eedd607c63b030c996a01db3
vancyferns <135420254+vancyferns@users.noreply.github.com>
2025-07-11 18:42:37 +0000

    spam detector completed

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from utils import model_predict
+app = Flask(__name__)
 
-app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    email = request.form.get('content')
+    prediction = model_predict(email)
+    return render_template("index.html", prediction=prediction, email=email)
+
+# Create an API endpoint
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
